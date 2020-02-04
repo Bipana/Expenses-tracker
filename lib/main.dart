3f9c3ed3a682fb46068a66eedebb2ebc6d3ef2cc
BipanaKarki <bipanakarki222@gmail.com>
2020-02-04 13:07:31 +0545

    this is expense tracker

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
-import 'package:expenses_tracker/Transaction.dart';
-import 'package:flutter/cupertino.dart';
+import 'package:expenses_tracker/models/Transaction.dart';
+import 'package:expenses_tracker/widgets/new_transaction.dart';
+
+import 'package:expenses_tracker/widgets/transaction_list.dart';
+
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';
 
 void main() => runApp(MyApp());
 
@@ -9,12 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("Expenses Tracker"),
-        ),
-        body: ExpensePage(),
-      ),
+      home: ExpensePage(),
     );
   }
 }
@@ -25,109 +22,68 @@ class ExpensePage extends StatefulWidget {
 }
 
 class _ExpensePageState extends State<ExpensePage> {
-  List<Transaction> transactions = [
+  List<Transaction> _usertransactions = [
     Transaction(
         itemName: "Groceries", itemPrice: 12.50, itemDate: DateTime.now()),
     Transaction(itemName: "Watch", itemPrice: 60.50, itemDate: DateTime.now()),
     Transaction(itemName: "Gown", itemPrice: 30.50, itemDate: DateTime.now()),
   ];
 
-  String itemName;
-  String itemPrice;
+  void _addNewTransaction(String txName, double txPrice) {
+    final newTx = Transaction(
+        itemName: txName, itemPrice: txPrice, itemDate: DateTime.now());
+    setState(() {
+      _usertransactions.add(newTx);
+    });
+  }
+
+  void _showAddTransaction(BuildContext bCtx) {
+    showModalBottomSheet(
+        context: bCtx,
+        builder: (ctx) {
+          return NewTransaction(_addNewTransaction);
+        });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: <Widget>[
-        Container(
-          width: double.infinity,
-          child: Card(
-            child: Text("Chart"),
-            elevation: 5,
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Expenses Tracker"),
+        centerTitle: true,
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.add, color: Colors.white),
+            onPressed: () {
+              _showAddTransaction(context);
+            },
           ),
-        ),
-        Card(
-          elevation: 5,
-          child: Container(
-            padding: EdgeInsets.all(10),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.end,
-              children: <Widget>[
-                TextField(
-                    decoration: InputDecoration(
-                      labelText: "Item Name",
-                    ),
-                    onChanged: (val) {
-                      itemName = val;
-                    }),
-                TextField(
-                    decoration: InputDecoration(
-                      labelText: "Item Price",
-                    ),
-                    onChanged: (val) {
-                      itemPrice = val;
-                    }),
-                RaisedButton(
-                  child: Text("Add Transaction"),
-                  textColor: Colors.white,
-                  onPressed: () {
-                    setState(() {
-                      transactions.add(Transaction(
-                          itemPrice: double.parse(itemPrice),
-                          itemDate: DateTime.now()));
-                    });
-                  },
-                )
-              ],
-            ),
-          ),
-        ),
-        Column(
-          children: transactions.map((tx) {
-            return Card(
-              child: Row(
-                children: <Widget>[
-                  Container(
-                    child: Text(
-                      '\$' + tx.itemPrice.toString(),
-                      style: TextStyle(
-                        color: Colors.green,
-                        fontSize: 30.0,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                    decoration: BoxDecoration(
-                        border: Border.all(
-                      color: Colors.green,
-                      width: 2,
-                    )),
-                    margin: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
-                    padding: EdgeInsets.all(5.0),
-                  ),
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      Text(
-                        tx.itemName,
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 25.0,
-                        ),
-                      ),
-                      Text(
-                        DateFormat.yMMMEd().format(tx.itemDate),
-                        style: TextStyle(
-                          fontSize: 15.0,
-                          color: Colors.grey,
-                        ),
-                      ),
-                    ],
-                  )
-                ],
+        ],
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            Container(
+              width: double.infinity,
+              child: Card(
+                child: Text("Chart"),
+                elevation: 5,
               ),
-            );
-          }).toList(),
-        )
-      ],
+            ),
+            Transactionlist(_usertransactions),
+          ],
+        ),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      floatingActionButton: FloatingActionButton(
+        child: Icon(
+          Icons.add,
+          color: Colors.white,
+        ),
+        onPressed: () {
+          _showAddTransaction(context);
+        },
+      ),
     );
   }
 }
---
+++ lib/widgets/transaction_list.dart
@@ -0,0 +1,63 @@
+import 'package:expenses_tracker/models/Transaction.dart';
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+class Transactionlist extends StatelessWidget {
+  final List<Transaction> transactions;
+
+  Transactionlist(this.transactions);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 300,
+      child: ListView.builder(
+          itemCount: transactions.length,
+          itemBuilder: (ctx, index) {
+            return Card(
+              child: Row(
+                children: <Widget>[
+                  Container(
+                    child: Text(
+                      '\$' + transactions[index].itemPrice.toString(),
+                      style: TextStyle(
+                        color: Colors.green,
+                        fontSize: 30.0,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    decoration: BoxDecoration(
+                        border: Border.all(
+                      color: Colors.green,
+                      width: 2,
+                    )),
+                    margin: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+                    padding: EdgeInsets.all(5.0),
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Text(
+                        transactions[index].itemName,
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 25.0,
+                        ),
+                      ),
+                      Text(
+                        DateFormat.yMMMEd()
+                            .format(transactions[index].itemDate),
+                        style: TextStyle(
+                          fontSize: 15.0,
+                          color: Colors.grey,
+                        ),
+                      ),
+                    ],
+                  )
+                ],
+              ),
+            );
+          }),
+    );
+  }
+}
