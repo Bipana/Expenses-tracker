182e56bbe9d22e388d3d5ac946fb731d78f8c321
BipanaKarki <bipanakarki222@gmail.com>
2020-02-12 14:45:44 +0545

    Completed cupertino adaptivness of the app and added cupertino fields

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,8 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
+import 'dart:io';
+
 void main() {
   /*---------this method ensures only to use portrait mode---------*/
 
@@ -96,80 +98,105 @@ class _ExpensePageState extends State<ExpensePage> {
 
   @override
   Widget build(BuildContext context) {
-    final isLandscape =
-        MediaQuery.of(context).orientation == Orientation.landscape;
+    final mediaQuery = MediaQuery.of(context);
 
-    final appBar = AppBar(
-      title: Text("Expenses Tracker"),
-      centerTitle: true,
-      actions: <Widget>[
-        IconButton(
-          icon: Icon(Icons.add, color: Colors.white),
-          onPressed: () {
-            _showAddTransaction(context);
-          },
-        ),
-      ],
+    final isLandscape = mediaQuery.orientation == Orientation.landscape;
+
+    final PreferredSizeWidget appBar = Platform.isIOS
+        ? CupertinoNavigationBar(
+            middle: Text("Expenses Tracker"),
+            trailing: Row(
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                GestureDetector(
+                  child: Icon(CupertinoIcons.add, color: Colors.white),
+                  onTap: () {
+                    _showAddTransaction(context);
+                  },
+                ),
+              ],
+            ),
+          )
+        : AppBar(
+            title: Text("Expenses Tracker"),
+            centerTitle: true,
+            actions: <Widget>[
+              IconButton(
+                icon: Icon(Icons.add, color: Colors.white),
+                onPressed: () {
+                  _showAddTransaction(context);
+                },
+              ),
+            ],
+          );
+
+    final pageBody = SingleChildScrollView(
+      child: Column(
+        children: <Widget>[
+          if (isLandscape)
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Text("Show Chart"),
+                Switch.adaptive(
+                  value: _showChart,
+                  onChanged: (value) {
+                    setState(() {
+                      _showChart = value;
+                    });
+                  },
+                ),
+              ],
+            ),
+          if (!isLandscape)
+            Container(
+                height: (mediaQuery.size.height * 0.4) -
+                    appBar.preferredSize.height -
+                    mediaQuery.padding.top,
+                child: Chart(_recentTransactions)),
+          if (!isLandscape)
+            Container(
+                height: (mediaQuery.size.height * 0.6) -
+                    appBar.preferredSize.height -
+                    mediaQuery.padding.top,
+                child: TransactionList(_userTransactions, _deleteTransaction)),
+          if (isLandscape)
+            _showChart
+                ? Container(
+                    height: (mediaQuery.size.height * 0.6) -
+                        appBar.preferredSize.height -
+                        mediaQuery.padding.top,
+                    child: Chart(_recentTransactions))
+                : Container(
+                    height: (mediaQuery.size.height * 0.6) -
+                        appBar.preferredSize.height -
+                        mediaQuery.padding.top,
+                    child:
+                        TransactionList(_userTransactions, _deleteTransaction)),
+        ],
+      ),
     );
-    return Scaffold(
-      appBar: appBar,
-      body: SingleChildScrollView(
-        child: Column(
-          children: <Widget>[
-            if (isLandscape)
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Text("Show Chart"),
-                  Switch(
-                    value: _showChart,
-                    onChanged: (value) {
-                      setState(() {
-                        _showChart = value;
-                      });
+    return Platform.isIOS
+        ? CupertinoPageScaffold(
+            child: pageBody,
+            navigationBar: appBar,
+          )
+        : Scaffold(
+            appBar: appBar,
+            body: pageBody,
+            floatingActionButtonLocation:
+                FloatingActionButtonLocation.centerFloat,
+            floatingActionButton: Platform.isIOS
+                ? Container()
+                : FloatingActionButton(
+                    child: Icon(
+                      Icons.add,
+                      color: Colors.white,
+                    ),
+                    onPressed: () {
+                      _showAddTransaction(context);
                     },
                   ),
-                ],
-              ),
-            if (!isLandscape)
-              Container(
-                  height: (MediaQuery.of(context).size.height * 0.4) -
-                      appBar.preferredSize.height -
-                      MediaQuery.of(context).padding.top,
-                  child: Chart(_recentTransactions)),
-            if (!isLandscape)
-              Container(
-                  height: (MediaQuery.of(context).size.height * 0.6) -
-                      appBar.preferredSize.height -
-                      MediaQuery.of(context).padding.top,
-                  child:
-                      TransactionList(_userTransactions, _deleteTransaction)),
-            if (isLandscape)
-              _showChart
-                  ? Container(
-                      height: (MediaQuery.of(context).size.height * 0.6) -
-                          appBar.preferredSize.height -
-                          MediaQuery.of(context).padding.top,
-                      child: Chart(_recentTransactions))
-                  : Container(
-                      height: (MediaQuery.of(context).size.height * 0.6) -
-                          appBar.preferredSize.height -
-                          MediaQuery.of(context).padding.top,
-                      child: TransactionList(
-                          _userTransactions, _deleteTransaction)),
-          ],
-        ),
-      ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
-      floatingActionButton: FloatingActionButton(
-        child: Icon(
-          Icons.add,
-          color: Colors.white,
-        ),
-        onPressed: () {
-          _showAddTransaction(context);
-        },
-      ),
-    );
+          );
   }
 }
--- lib/widgets/new_transaction.dart
+++ lib/widgets/new_transaction.dart
@@ -1,5 +1,8 @@
+import 'package:expenses_tracker/widgets/adaptive_button.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
+import 'dart:io';
 
 class NewTransaction extends StatefulWidget {
   final Function addTx;
@@ -24,16 +27,27 @@ class _NewTransactionState extends State<NewTransaction> {
   }
 
   void _pickDate() {
-    showDatePicker(
-            context: context,
-            initialDate: DateTime.now(),
-            firstDate: DateTime(2020),
-            lastDate: DateTime.now())
-        .then((pickedDate) {
-      setState(() {
-        _selectedDateTime = pickedDate;
-      });
-    });
+    Platform.isIOS
+        ? CupertinoDatePicker(
+            initialDateTime: DateTime.now(),
+            maximumDate: DateTime.now(),
+            minimumDate: DateTime(2020),
+            onDateTimeChanged: (pickedDate) {
+              setState(() {
+                _selectedDateTime = pickedDate;
+              });
+            },
+          )
+        : showDatePicker(
+                context: context,
+                initialDate: DateTime.now(),
+                firstDate: DateTime(2020),
+                lastDate: DateTime.now())
+            .then((pickedDate) {
+            setState(() {
+              _selectedDateTime = pickedDate;
+            });
+          });
   }
 
   @override
@@ -79,15 +93,10 @@ class _NewTransactionState extends State<NewTransaction> {
                         style: Theme.of(context).textTheme.title.copyWith(
                             fontSize: 16, fontWeight: FontWeight.normal)),
                   ),
-                  FlatButton(
-                    child: Text(
-                      "Choose a date",
-                      style: TextStyle(
-                          color: Theme.of(context).primaryColor,
-                          fontWeight: FontWeight.bold),
-                    ),
-                    onPressed: _pickDate,
-                  )
+                  AdaptiveButton(
+                    text: "Choose a date",
+                    onPress: _pickDate,
+                  ),
                 ],
               ),
               RaisedButton(
